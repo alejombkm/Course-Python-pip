e7ff3f1851e36e176936a1552b9b0063b299332f
Alejandro Molina <alejombkm01@gmail.com>
2023-08-12 09:36:07 -0500

    update files

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import styore
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Hello i am a page</h1>
+        <p>I am a paragraph<p>
+    """
 
 def run():
     styore.get_categories()
